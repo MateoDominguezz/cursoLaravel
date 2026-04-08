120d87f9a953e16bc929ee7e4402cc4df03aa139
Mateo Dominguez <mateodominguez83@yahoo.com>
2026-04-08 11:29:41 -0300

    Viendo sobre Events y Listeners

---
+++ events-example/app/Listeners/GenerateInvoiceListener.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\CreateOrderEvent;
+use App\Models\Invoice;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class GenerateInvoiceListener
+{
+
+    public function __construct() {}
+
+    public function handle(CreateOrderEvent $event): void
+    {
+        Invoice::create([
+            "amount" => $event->order->amount,
+            "order_id" => $event->order->id
+        ]);
+    }
+}
---
+++ events-example/routes/web.php
@@ -0,0 +1,6 @@
+<?php
+
+use App\Http\Controllers\OrderController;
+use Illuminate\Support\Facades\Route;
+
+Route::get("/create", [OrderController::class, "create"])->name("order.create");
